ced84666a45e45bb46c0e3dfa8fdedb47280c48f
Denis Gordin <gordin.dan@gmail.com>
2019-12-27 14:56:19 -0500

    #

--- photo_booth/lib/models/main_model.dart
+++ photo_booth/lib/models/main_model.dart
@@ -1,6 +1,10 @@
 import 'dart:io';
-import 'dart:ui';
+import 'dart:typed_data';
+import 'dart:ui' as ui;
 
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:gallery_saver/gallery_saver.dart';
 import 'package:image_picker/image_picker.dart' as imagePicker;
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
@@ -8,8 +12,19 @@ import 'package:scoped_model/scoped_model.dart';
 
 class MainModel extends Model {
 
-  CameraPreviewState previewState = CameraPreviewState.empty;
+  GlobalKey canvasKey;
 
+  Color selectedColor = Colors.black;
+  bool showColorsWidget = false;
+  List<Color> colors = [
+    Colors.red,
+    Colors.green,
+    Colors.blue,
+    Colors.amber,
+    Colors.black
+  ];
+
+  CameraPreviewState previewState = CameraPreviewState.empty;
 
   String _currentImagePath;
   String get currentImagePath => _currentImagePath;
@@ -27,12 +42,18 @@ class MainModel extends Model {
   }
 
   void undo() {
-    if (_points == null || _points.length <= 1)
+    if (_points.isEmpty)
       return;
-    do {
-      _points.removeLast();
-    } while (_points.last != null);
-    notifyListeners();
+
+    try {      
+      do {
+        if (_points.isNotEmpty)
+          _points.removeLast();
+      } while (_points.last != null);
+      notifyListeners();
+    } catch (e) {
+      notifyListeners();
+    }
   }
 
   void clear() {
@@ -62,6 +83,48 @@ class MainModel extends Model {
     setPreviewState(CameraPreviewState.image);
     notifyListeners();
   }
+
+  Future<bool> saveImage(ImageFormat imageFormat) async {
+    if (currentImagePath.isEmpty) {
+      print('temp image path is empty');
+      return false;
+    }
+    
+    if (canvasKey == null){
+      print('canvas key is null');
+      return false;
+    }
+
+    switch (imageFormat) {
+      case ImageFormat.png:
+        return await _savePng(canvasKey);
+      default:
+        return false;
+    }
+  }
+
+  Future<bool> _savePng(GlobalKey key) async {
+    try {
+      // finding canvas of the widget
+      RenderRepaintBoundary boundary = key.currentContext.findRenderObject();
+      // transform the canvas into image
+      ui.Image image = await boundary.toImage(pixelRatio: 3.0);
+      // convert the image into png
+      ByteData byteData = await image.toByteData(format: ui.ImageByteFormat.png);
+      // transfering png as sequence of bytes
+      Uint8List pngBytes = byteData.buffer.asUint8List();
+      // making temp file
+      final path = join((await getTemporaryDirectory()).path, 'canvas_${DateTime.now()}.png');
+      var tempfile = File(path);
+      // write bytes into the file
+      tempfile.writeAsBytesSync(pngBytes);
+      // saving temp file into OS gallery
+      return await GallerySaver.saveImage(tempfile.path);
+    } catch (e) {
+      print(e);
+      return false;
+    }
+  }
 }
 
 class DrawingPoint {
@@ -73,4 +136,5 @@ class DrawingPoint {
 
 enum SelectedMode { StrokeWidth, Opacity, Color }
 enum CameraPreviewState { empty, image }
+enum ImageFormat { png, jpeg, photobooth }
 // enum ImageSource { fromFile, fromCamera }
